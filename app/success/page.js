8ec59761f67931e3a4fb1ae4eef97fb00ae40a80
HazWahb8080 <hazem.wahb100@gmail.com>
2024-02-17 17:11:51 +0200

    fix

--- app/success/page.js
+++ app/success/page.js
@@ -11,7 +11,7 @@ import {
 } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 
 function SuccessPage() {
   const router = useRouter();
@@ -70,35 +70,41 @@ function SuccessPage() {
   }, [session_id, session]);
   if (!session || !session_id) {
     return (
-      <div
-        className="w-full min-h-screen
+      <Suspense>
+        <div
+          className="w-full min-h-screen
        bg-black text-white items-center  justify-center flex flex-col"
-      >
-        loading ....
-      </div>
+        >
+          loading ....
+        </div>
+      </Suspense>
     );
   }
   if (message === "error" && message !== "") {
     return (
-      <div
-        className="w-full min-h-screen
+      <Suspense>
+        <div
+          className="w-full min-h-screen
        bg-black text-white items-center  justify-center flex flex-col"
-      >
-        <h1> There is an error validating your payment details.</h1>
-      </div>
+        >
+          <h1> There is an error validating your payment details.</h1>
+        </div>
+      </Suspense>
     );
   }
   if (message === "success") {
     return (
-      <div
-        className="w-full min-h-screen
+      <Suspense>
+        <div
+          className="w-full min-h-screen
        bg-black text-white items-center  justify-center flex flex-col"
-      >
-        <h1 className="text-2xl">
-          Thanks for your purchase!,{session?.user?.name}!
-        </h1>
-        <p>redirecting now ...</p>
-      </div>
+        >
+          <h1 className="text-2xl">
+            Thanks for your purchase!,{session?.user?.name}!
+          </h1>
+          <p>redirecting now ...</p>
+        </div>
+      </Suspense>
     );
   }
 }
